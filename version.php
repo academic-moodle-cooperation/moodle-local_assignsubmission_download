3a990998b25438fb1d26fd0bf41095c07d19a325
Andreas Krieger <Andreas.Krieger@tuwien.ac.at>
2018-12-07 18:52:01 +0100

    #5798 Add langstring [de] to plugin

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018120600;
+$plugin->version   = 2018120700;
 $plugin->requires  = 2018112800;           // Requires this Moodle version!
 $plugin->component = 'local_assignsubmission_download';    // To check on upgrade, that module sits in correct place.
 $plugin->maturity  = MATURITY_BETA;
