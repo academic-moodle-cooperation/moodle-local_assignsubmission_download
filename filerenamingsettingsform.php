347626f83b6c9faf9b8fa99fd08663982fba6be4
Clemens <clemens.marx@gmail.com>
2024-03-27 17:28:18 +0100

    #7256 Edit JavaScript module call

--- filerenamingsettingsform.php
+++ filerenamingsettingsform.php
@@ -199,7 +199,7 @@ class mod_assign_filerenaming_settings_form extends moodleform {
                 get_string('rename_propertydescription', 'local_assignsubmission_download', implode("", $ziprenamingtags)));
         $mform->addHelpButton('nameofziparchive', 'nameofziparchive', 'local_assignsubmission_download');
 
-        $PAGE->requires->js_call_amd('local_assignsubmission_download/ziprenaming_tagsupport', 'initializer', []);
+        $PAGE->requires->js_call_amd('local_assignsubmission_download/ziprenaming_tagsupport', 'init', []);
         $PAGE->requires->js_call_amd('local_assignsubmission_download/filerenaming_groupingtoggle', 'initializer', [$jsgroupings]);
 
         $mform->addElement('hidden', 'contextid', $instance['contextid']);
