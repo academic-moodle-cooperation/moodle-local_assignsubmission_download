2b44bd24c4f9422535458556f35c41276a91dd74
Clemens <clemens.marx@gmail.com>
2025-01-08 15:32:17 +0100

    #8110 Fix dynamic property deprecation

--- mtablepdf.php
+++ mtablepdf.php
@@ -93,6 +93,8 @@ class MTablePDF extends \pdf {
 
     /** @var $data tables data */
     private $data = [];
+    /** @var $header header titles */
+    private $header = [];
 
     /**
      * Constructor
