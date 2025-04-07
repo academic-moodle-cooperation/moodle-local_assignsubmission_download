00051123ee694a9395e884ce56bab70a01c91786
Clemens <clemens.marx@gmail.com>
2025-04-07 23:25:41 +0200

    #8308 Finish 4.5.2; fix CSV download issue with ';'

--- version.php
+++ version.php
@@ -25,11 +25,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024100401;
+$plugin->version   = 2024100402;
 $plugin->requires  = 2024100700;           // Requires this Moodle version!
 $plugin->component = 'local_assignsubmission_download';    // To check on upgrade, that module sits in correct place.
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v4.5.1';            // User-friendly version number.
+$plugin->release   = 'v4.5.2';            // User-friendly version number.
 
 $plugin->dependencies = [
     'mod_assign' => 2021110900,
