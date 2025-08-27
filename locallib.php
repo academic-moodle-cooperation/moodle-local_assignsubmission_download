897662146d49239d250d37ed1dd01d7c26c9fb47
Julien Boulen <julien.boulen@univ-rennes2.fr>
2025-08-27 11:23:47 +0200

    Convert upper unicode characters to plain ascii before cleaning up file names

--- locallib.php
+++ locallib.php
@@ -225,6 +225,7 @@ function clean_custom($filename) {
 
     $cleanfilenameuserpref = get_user_preferences('clean_filerenaming', '');
     if ((isset($cleanfilenameuserpref) && $cleanfilenameuserpref)) {
+        $filename = core_text::specialtoascii($filename);
         $filename = preg_replace('/[^A-Za-z0-9\_\-\.]/', '', $filename);
     }
     return clean_filename($filename);
