281dec673d54da8238cd250ce0b8ecd47642d37f
Clemens Marx <clemens.marx@gmail.com>
2025-12-17 10:40:58 +0100

    #8605 Change formatting with CBF to fix pipeline issues

--- printpreviewsettingsform.php
+++ printpreviewsettingsform.php
@@ -66,9 +66,18 @@ class mod_assign_printpreview_settings_form extends moodleform {
             $dirtyclass
         );
 
-        $mform->addElement('static', 'onlypdfsettings', '',
-                html_writer::div(html_writer::span(
-                    get_string('onlypdf', 'local_assignsubmission_download'), null), 'bold'));
+        $mform->addElement(
+            'static',
+            'onlypdfsettings',
+            '',
+            html_writer::div(
+                html_writer::span(
+                    get_string('onlypdf', 'local_assignsubmission_download'),
+                    null
+                ),
+                'bold'
+            )
+        );
         $mform->hideIf('onlypdfsettings', 'exportformat', 'neq', MTablePDF::OUTPUT_FORMAT_PDF);
 
         $grpperpage = [];
@@ -102,16 +111,26 @@ class mod_assign_printpreview_settings_form extends moodleform {
             1 => get_string('strmedium', 'local_assignsubmission_download'),
             2 => get_string('strlarge', 'local_assignsubmission_download'),
         ];
-        $mform->addElement('select', 'textsize',
-            get_string('strtextsize', 'local_assignsubmission_download'), $options, $dirtyclass);
+        $mform->addElement(
+            'select',
+            'textsize',
+            get_string('strtextsize', 'local_assignsubmission_download'),
+            $options,
+            $dirtyclass
+        );
         $mform->hideIf('textsize', 'exportformat', 'neq', MTablePDF::OUTPUT_FORMAT_PDF);
 
         $options = [
             0 => get_string('strportrait', 'local_assignsubmission_download'),
             1 => get_string('strlandscape', 'local_assignsubmission_download'),
         ];
-        $mform->addElement('select', 'pageorientation',
-            get_string('strpageorientation', 'local_assignsubmission_download'), $options, $dirtyclass);
+        $mform->addElement(
+            'select',
+            'pageorientation',
+            get_string('strpageorientation', 'local_assignsubmission_download'),
+            $options,
+            $dirtyclass
+        );
         $mform->hideIf('pageorientation', 'exportformat', 'neq', MTablePDF::OUTPUT_FORMAT_PDF);
 
         $mform->addElement('advcheckbox', 'printheader', get_string('strprintheader', 'local_assignsubmission_download'), ' ');
